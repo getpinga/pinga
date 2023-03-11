2de6a8798a2931305a4f6910255077af16b56952
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-12 00:18:21 +0200

    Update CustomerController.php

--- src/Controllers/CustomerController.php
+++ src/Controllers/CustomerController.php
@@ -7,9 +7,9 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class CustomerController implements RequestHandlerInterface
+class CustomerController
 {
-    public function handle(ServerRequestInterface $request): ResponseInterface
+    public function index(ServerRequestInterface $request): ResponseInterface
     {
         echo PHP_EOL."ParsedBody: ".PHP_EOL;var_dump($request->getParsedBody());echo PHP_EOL;
         echo PHP_EOL."UploadedFiles: ".PHP_EOL;var_dump($request->getUploadedFiles());echo PHP_EOL;
