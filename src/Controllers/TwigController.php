1b87ea1b8f036cc6a1511dc16a3f9ebc1e9a7330
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-14 18:17:47 +0200

    Added a view rendering system

---
+++ src/Controllers/TwigController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\TwigWrapper;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class TwigController
+{
+    private TwigWrapper $twig;
+
+    public function __construct()
+    {
+        $templateDir = __DIR__ . '/../../views';
+        $this->twig = new TwigWrapper($templateDir);
+    }
+
+    public function index(\Psr\Http\Message\ServerRequestInterface $request): ResponseInterface
+    {
+        $responseBody = $this->twig->render('test.twig', ['name' => 'Foo']);
+
+        $contentLength = strlen($responseBody);
+
+        return new Response(
+            200,
+            [
+                'Content-Type' => 'text/html',
+                'Content-Length' => $contentLength,
+                'Date' => gmdate('D, d M Y H:i:s').' GMT',
+                'Server' => 'Pinglet',
+                'Cache-Control' => 'max-age=3600',
+                'Access-Control-Allow-Origin' => '*'
+            ],
+            $responseBody
+        );
+    }
+}
