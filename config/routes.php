f3b88b8768d7b618b3480fa88b43b28db0a0ea17
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-14 18:16:28 +0200

    Added a view rendering system

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Controllers\PlatesController;
+use App\Controllers\TwigController;
 use App\Controllers\CustomerController;
 use App\Controllers\IndexController;
 use FastRoute\RouteCollector;
@@ -8,6 +10,8 @@ use Psr\Http\Message\ServerRequestInterface;
 
 $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
     $r->get('/test', [IndexController::class, 'index']);
+    $r->get('/plates', [PlatesController::class, 'index']);
+    $r->get('/twig', [TwigController::class, 'index']);
     $r->post('/test', [CustomerController::class, 'index']);
     $r->get('/test/{id}', function (ServerRequestInterface $request, array $args) {
         return new Response(
