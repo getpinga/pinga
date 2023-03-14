3f6d091b55b58073aeea0e553af025cba663c3b0
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-14 18:28:36 +0200

    Update start-swow-http.php

--- start-swow-http.php
+++ start-swow-http.php
@@ -14,13 +14,18 @@ use Swow\SocketException;
 use App\PiLogger;
 use FastRoute\Dispatcher;
 use Nyholm\Psr7\ServerRequest;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . "/vendor/autoload.php";
 $routeCollection = require __DIR__ . "/config/routes.php";
 
-$host = "0.0.0.0";
-$hostname = "my.host.com";
-$port = 8081;
+// Load environment variables from .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['HOST'];
+$hostname = $_ENV['HOSTNAME'];
+$port = intval($_ENV['PORT']);
 
 $server = new \Swow\Psr7\Server\Server();
 $server->bind($host, $port)->listen(Socket::DEFAULT_BACKLOG);
