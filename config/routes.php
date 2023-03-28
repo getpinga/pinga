b33dd6e0890de7e6d80c2098a558acaee8f8571f
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-28 17:11:31 +0300

    Bugfixes

--- config/routes.php
+++ config/routes.php
@@ -2,23 +2,18 @@
 
 use App\Controllers\PlatesController;
 use App\Controllers\TwigController;
-use App\Controllers\CustomerController;
+use App\Controllers\UserController;
 use App\Controllers\IndexController;
 use FastRoute\RouteCollector;
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
-    $r->get('/test', [IndexController::class, 'index']);
+    $r->get('/', [IndexController::class, 'index']);
+    $r->get('/user/{id}', [UserController::class, 'getUser']);
+    $r->post('/user', [UserController::class, 'index']);
     $r->get('/plates', [PlatesController::class, 'index']);
     $r->get('/twig', [TwigController::class, 'index']);
-    $r->post('/test', [CustomerController::class, 'index']);
-    $r->get('/test/{id}', function (ServerRequestInterface $request, array $args) {
-        return new Response(
-            status: 201,
-            body: $args['id']
-        );
-    });
 });
 
 return $dispatcher;
